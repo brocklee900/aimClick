ee453270c572c9924fefeb2ea5382bbf0485c8f1
Vincent Tran <83623108+brocklee900@users.noreply.github.com>
2025-05-16 16:11:18 -0700

    Add function to randomly spawn target in playboard

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 console.log("AimClick");
+
+let playboard = document.querySelector(".playBoard");
+
+function createTarget() {
+    let target = document.createElement("div");
+    target.classList.add("target");
+    
+    target.style.height = .1*playboard.clientHeight + 'px';
+    target.style.width = .1*playboard.clientWidth + 'px';
+    target.style.top = (Math.random() * (90)) + '%';
+    target.style.left = (Math.random() * (90)) + '%';
+
+    playboard.appendChild(target);
+}
+
+let startButton = document.querySelector("#start");
+
+startButton.addEventListener("click", () => {
+    createTarget();
+})
